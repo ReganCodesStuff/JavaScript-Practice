809bba7c3bc0b783015632cee52f5a641d5805d7
Regan Smith <regansmith10@outlook.com>
2023-08-04 12:00:24 +0100

    updated

--- sandbox.js
+++ sandbox.js
@@ -48,14 +48,74 @@
 
 /// ----- ///
 
-const link = document.querySelector("a");
+// const link = document.querySelector("a");
 
-console.log(link.getAttribute("href"));
-link.setAttribute("href", "https://www.thenetninja.co.uk")
-link.innerText = "The Net Ninja Website";
+// console.log(link.getAttribute("href"));
+// link.setAttribute("href", "https://www.thenetninja.co.uk")
+// link.innerText = "The Net Ninja Website";
 
-const mssg = document.querySelector("p");
+// const mssg = document.querySelector("p");
 
-console.log(mssg.getAttribute("class"));
-mssg.setAttribute("class", "success");
-mssg.setAttribute("style", "color: green");
+// console.log(mssg.getAttribute("class"));
+// mssg.setAttribute("class", "success");
+// mssg.setAttribute("style", "color: green");
+
+/// ----- ///
+
+// const title = document.querySelector("h1");
+
+// title.setAttribute("style", "margin: 50px");
+
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = "50px"
+// title.style.color = "crimson"
+// title.style.fontSize = "60px"
+// title.style.margin = ""
+
+/// ----- ///
+
+// const content = document.querySelector("p");
+
+// console.log(content.classList);
+// content.classList.add("error");
+// content.classList.remove("remove");
+// content.class.add("success");
+
+// const paras = document.querySelector("p");
+
+// paras.forEach(p => {
+//     if(p.textContent.includes("error")){
+//         p.classList.add("error");
+//     }
+//     if(p.textContent.includes("success")){
+//         p.classList.add("success");
+//     }
+// });
+
+// const title = document.querySelector(".title");
+
+// title.classList.toggle("test");
+
+/// ----- ///
+
+const article = document.querySelector("article");
+
+// console.log(article.children);
+// console.log(Array.from(article.children));
+// console.log(article.children);
+
+// Array.from(article.children).forEach(child => {
+//   child.classList.add("article-element");
+// });
+
+// const title = document.querySelector("h2");
+
+// console.log(title.parentElement);
+// console.log(title.parentElement.parentElement);
+// console.log(title.nextElementSibling);
+// console.log(title.previousElementSibling);
+
+// chaining
+// console.log(title.nextElementSibling.parentElement.children);
